3f1f3d917858e908a2841f57838ef62c26732bf6
Jeremy Kepf <jeremy1975@gmail.com>
2019-11-05 20:41:43 -0500

    fixed env config on local and netlify side

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,14 @@ import * as firebase from 'firebase';
 Vue.config.productionTip = false;
 
 const configOptions = {
-  apiKey: 'AIzaSyBKxBBOGS9FIIdWPPKRBAG8hJYmSOIN7Pg',
-  authDomain: 'playing-time-tracker-73665.firebaseapp.com',
-  databaseURL: 'https://playing-time-tracker-73665.firebaseio.com',
-  projectId: 'playing-time-tracker-73665',
-  storageBucket: 'playing-time-tracker-73665.appspot.com',
-  messagingSenderId: '825843284810',
-  appId: '1:825843284810:web:4ac6f4ed4a88bffb3d9fb3',
-  measurementId: 'G-3BNQ2SD295'
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(configOptions);
