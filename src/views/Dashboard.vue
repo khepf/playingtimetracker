803a6eff2a95009e6df69255f9cbe8368bd1982a
Jeremy Kepf <jeremy1975@gmail.com>
2019-11-08 14:55:38 -0500

    new message , new player now able to be saved to db and displayed on dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,6 +12,22 @@
               <h3>{{ player.lastName }}</h3>
               <h3>{{ player.jerseyNumber }}</h3>
             </div>
+            <h1>Add a Player</h1>
+            <form @submit.prevent="storePlayer">
+              <div class="form-group">
+                <label>First Name</label>
+                <input v-model="firstName" class="form-control"/>
+              </div>
+              <div class="form-group">
+                <label>Last Name</label>
+                <input v-model="lastName" class="form-control"/>
+              </div>
+              <div class="form-group">
+                <label>Jersey Number</label>
+                <input v-model="jerseyNumber" class="form-control"/>
+              </div>
+              <button>Send</button>
+            </form>
             <h1>List of Messages</h1>
             <div v-for="message in messages">
               <h3>{{ message.nickname }}</h3>
@@ -53,11 +69,19 @@ export default {
   },
   methods: {
     storeMessage() {
-      this.messages.push({text: this.messageText, nickname: this.nickname});
+      const database = firebase.database();
+      database.ref('messages').push({text: this.messageText, nickname: this.nickname});
       this.messageText = '';
+      this.nickname = '';
     },
     storePlayer() {
-      this.players.push({firstName: this.firstName, lastName: this.lastName, jerseyNumber: this.jerseyNumber});
+      const database = firebase.database();
+      database.ref('players').push(
+        {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          jerseyNumber: this.jerseyNumber
+        });
       this.firstName = '';
       this.lastName = '';
       this.jerseyNumber = '';
